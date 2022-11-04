b04d37e9779efa0947a29134678ba416cf1f1e75
Mainak <saha_mainak@hotmail.com>
2022-11-04 00:46:40 +0000

    test
    test

---
+++ AppInsightService/AppInsightService/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+app.UseSwagger();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
