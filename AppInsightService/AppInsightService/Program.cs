5d4c3abfdd11ca638cc1efe2819606dd158f7006
Mainak <saha_mainak@hotmail.com>
2022-11-06 00:45:16 +0000

    adaptive sampling configuration added

--- AppInsightService/AppInsightService/Program.cs
+++ AppInsightService/AppInsightService/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.ApplicationInsights.Extensibility;
-
+using Microsoft.ApplicationInsights.WindowsServer.Channel.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,14 +11,27 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var aiOptions = new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions();
 
-//builder.Services.Configure<TelemetryConfiguration>((config) =>
-//{
-//    var tpBuilder = TelemetryConfiguration.Active.DefaultTelemetrySink.TelemetryProcessorChainBuilder;
-//    tpBuilder.UseAdaptiveSampling(maxTelemetryItemsPerSecond: 1, excludedTypes: "Exception");
-//    tpBuilder.Build();
-//});
+void AdaptiveTelCallback(double afterSamplingTelemetryItemRatePerSecond, double currentSamplingPercentage, double newSamplingPercentage, bool isSamplingPercentageChanged, SamplingPercentageEstimatorSettings settings)
+{ }
 
-aiOptions.EnableAdaptiveSampling = false;
+builder.Services.Configure<TelemetryConfiguration>((config) =>
+{
+    SamplingPercentageEstimatorSettings samplingPercentageEstimatorSettings = new SamplingPercentageEstimatorSettings();
+    samplingPercentageEstimatorSettings.EvaluationInterval = new TimeSpan(0, 0, 15);
+    samplingPercentageEstimatorSettings.InitialSamplingPercentage = 100;
+    samplingPercentageEstimatorSettings.MovingAverageRatio = 0.90; //Highly reactive to sudden changes
+    samplingPercentageEstimatorSettings.SamplingPercentageIncreaseTimeout = new TimeSpan(0,0,15);
+    samplingPercentageEstimatorSettings.EvaluationInterval = new TimeSpan(0, 0, 10); //shorten the interval to catch the certain burst
+    samplingPercentageEstimatorSettings.MaxTelemetryItemsPerSecond = 1;
+    AdaptiveSamplingPercentageEvaluatedCallback adaptiveSamplingPercentageEvaluatedCallback = AdaptiveTelCallback;
+
+    var tpBuilder = TelemetryConfiguration.Active.DefaultTelemetrySink.TelemetryProcessorChainBuilder;
+    //tpBuilder.UseAdaptiveSampling(maxTelemetryItemsPerSecond: 1, excludedTypes: "Exception");
+    tpBuilder.UseAdaptiveSampling(samplingPercentageEstimatorSettings, adaptiveSamplingPercentageEvaluatedCallback, excludedTypes: "Exception", includedTypes: "Request;Trace");
+    tpBuilder.Build();
+});
+
+aiOptions.EnableAdaptiveSampling = true;
 builder.Services.AddApplicationInsightsTelemetry(aiOptions);
 
 var app = builder.Build();
